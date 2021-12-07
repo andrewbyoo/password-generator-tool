07e28eeeef296322478b697880da201f63df625a
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-07 17:19:28 -0500

    Testing which variable in the starter code is the final password that appears on the html

--- script.js
+++ script.js
@@ -6,47 +6,42 @@ var lttrUppr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 var spclChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "{", "]", "}", "|", ";", ":", "'", '"', "<", ",", ">", ".", "`", "~", "/", "?"]
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-// ---------
-// TEST CODE
-// ---------
-var passLength = 12
-var allOptions = lttrLwr.concat(lttrUppr, spclChar, numbers)
-var outputPass = ""
+// // ---------
+// // TEST CODE
+// // ---------
+// var passLength = 12
+// var allOptions = lttrLwr.concat(lttrUppr, spclChar, numbers)
+// var outputPass = ""
 
-console.log(allOptions)
+// console.log(allOptions)
 
-// Function for generating password
-function generatePassword() {
-  // For length of password chosen, keep adding a new entry to the output
-  for (var i = 0; i < passLength; i++) {
-    // Assigns the formula to outputPass and outputs the resulting variable
-    // allOptions[integer number of the character to chose to add]
-    // Math.floor to round random output of math random
-    // allOptions.length to set the max possible random number
-    outputPass += allOptions[Math.floor(Math.random() * allOptions.length)];
-    console.log(outputPass)
-  } return outputPass
-}
-generatePassword();
-// ----------------
-// END OF TEST CODE
-// ----------------
+// // Function for generating password
+// function generatePassword() {
+//   // For length of password chosen, keep adding a new entry to the output
+//   for (var i = 0; i < passLength; i++) {
+//     // Assigns the formula to outputPass and outputs the resulting variable
+//     // allOptions[integer number of the character to chose to add]
+//     // Math.floor to round random output of math random
+//     // allOptions.length to set the max possible random number
+//     outputPass += allOptions[Math.floor(Math.random() * allOptions.length)];
+//     console.log(outputPass)
+//   } return outputPass
+// }
+// generatePassword();
+// // ----------------
+// // END OF TEST CODE
+// // ----------------
 
-
-
+// Code needed to get password onto html page
+// var password is the final password text after the randomization has completed
+var password = "abc123Test"
 
 // // Write password to the #password input
-// function generatePassword() {
-//   return
-// }
-
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
+function writePassword() {
+  // var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
