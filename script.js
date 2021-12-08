9147aba7d32dc347e8382c78a1a00daef7e82850
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-08 15:16:28 -0500

    Removed test code and edited some comments

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var spclChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+",
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var outputPass = ""
 
-// Option outputs
+// Option outputs not including the arrays on their own
 var allOptions = lttrLwr.concat(lttrUppr, spclChar, numbers)
 var options1 = lttrLwr.concat(lttrUppr, spclChar)
 var options2 = lttrLwr.concat(lttrUppr, numbers)
@@ -152,35 +152,6 @@ function generatePassword() {
     } return outputPass
   }
 }
-// // ---------
-// // TEST CODE
-// // ---------
-// var passLength = 12
-// var allOptions = lttrLwr.concat(lttrUppr, spclChar, numbers)
-// var outputPass = ""
-
-// console.log(allOptions)
-
-// // Function for generating password
-// function generatePassword() {
-//   // For length of password chosen, keep adding a new entry to the output
-//   for (var i = 0; i < passLength; i++) {
-//     // Assigns the formula to outputPass and outputs the resulting variable
-//     // allOptions[integer number of the character to chose to add]
-//     // Math.floor to round random output of math random
-//     // allOptions.length to set the max possible random number
-//     outputPass += allOptions[Math.floor(Math.random() * allOptions.length)];
-//     console.log(outputPass)
-//   } return outputPass
-// }
-// generatePassword();
-// // ----------------
-// // END OF TEST CODE
-// // ----------------
-
-// Code needed to get password onto html page
-// var password is the final password text after the randomization has completed
-// var password = "abc123Test"
 
 // Write password to the #password input
 function writePassword() {
@@ -189,5 +160,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
